a7627a41dacb6681015a572c0ce5d10513bb0cf5
lindorsito <santiacevedo@gmail.com>
2015-11-17 10:42:28 -0300

    asdfads
    asdfasd

--- Src/Codigo/GestionAdministrativa.Data/GestionAdministrativaModel.Designer.cs
+++ Src/Codigo/GestionAdministrativa.Data/GestionAdministrativaModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\Santy Rock\Documents\Trabajos\Otros\Apipe\GestionAdministrativa\Src\Codigo\GestionAdministrativa.Data\GestionAdministrativaModel.edmx'. 
+﻿// T4 code generation is enabled for model 'E:\Santy Rock\Documents\Trabajos\Otros\Apipe\Cobranza\GestionAdministrativa\Src\Codigo\GestionAdministrativa.Data\GestionAdministrativaModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
